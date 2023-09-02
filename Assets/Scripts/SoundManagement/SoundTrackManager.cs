f38bfc0ce467fe9e6f52090b191d8a96ccf4f020
deniz <halimi@joboo.de>
2023-09-02 20:35:47 +0200

    Add soundtrack
    - Made SoundTrackManager access AudioSource directly via component
    - Additional null-check

--- Assets/Scripts/SoundManagement/SoundTrackManager.cs
+++ Assets/Scripts/SoundManagement/SoundTrackManager.cs
@@ -6,11 +6,16 @@ namespace SoundManagement
 {
     public class SoundTrackManager : MonoBehaviour
     {
-        [SerializeField] AudioSource _soundTrack;
+        private AudioSource _soundTrack;
+
+        void Awake()
+        {
+            _soundTrack = GetComponent<AudioSource>();
+        }
 
         void Start()
         {
-            if (_soundTrack != null) {
+            if (_soundTrack != null && _soundTrack.audioClip != null) {
                 _soundTrack.Play();
             }
         }
